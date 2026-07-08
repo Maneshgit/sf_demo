6f7167d6e06feab671e422f470ce2aa0704695c2
Maneshgit <126378643+Maneshgit@users.noreply.github.com>
2026-07-08 20:03:52 +0530

    Update 001_add_email_to_customers.sql

--- 001_add_email_to_customers.sql
+++ 001_add_email_to_customers.sql
@@ -1,4 +1,4 @@
 -- Example migration: add email column to customers
--- Co-authored with CoCo
+-- Co-authored with CoComan
 ALTER TABLE IF EXISTS raw.jaffle_shop.customers
   ADD COLUMN IF NOT EXISTS email VARCHAR(256);
